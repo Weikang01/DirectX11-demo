ae20e354cf252e59aa98875ef0d1e0e1a90dbf59
Weikang01 <lwkdota@qq.com>
2021-08-10 05:26:47 -0400

    DxgiInfoManager part01

--- DirectX11Tutorial/DxgiInfoManager.h
+++ DirectX11Tutorial/DxgiInfoManager.h
@@ -1,16 +1,19 @@
 #pragma once
 #include "lib_Win.h"
+#include <wrl.h>
+#include <string>
 #include <vector>
+#include <dxgidebug.h>
 
 class DxgiInfoManager
 {
 public:
 	DxgiInfoManager();
-	~DxgiInfoManager();
+	~DxgiInfoManager() = default;
 	DxgiInfoManager(const DxgiInfoManager&) = delete;
 	DxgiInfoManager& operator=(const DxgiInfoManager&) = delete;
 	void Set() noexcept;
-	std::vector<std::strong_ordering> GetMessage() const;
+	std::vector<std::string> GetMessages() const;
 
 private:
 	unsigned long long next = 0u;
